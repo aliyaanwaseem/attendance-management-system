53d5a2303a93f724bf166276a3cef83ba1fda006
aliyaanwaseem <aliyaanwaseem56@gmail.com>
2026-05-29 17:48:59 +0500

    Removed test files

--- test_db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', '', 'attendance_db');
-
-if ($conn) {
-    echo "✅ Database connected on port 3306!";
-} else {
-    echo "❌ Connection failed: " . mysqli_connect_error();
-}
-?>
